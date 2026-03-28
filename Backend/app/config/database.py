8e6aca402b9bb7219935a9788187b38c6a9b0540
Laukik Waikar <157301589+LAU29004@users.noreply.github.com>
2026-03-28 23:13:38 +0530

    .env used

--- Backend/app/config/database.py
+++ Backend/app/config/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:Nox%40123456789@localhost:5432/legislative_ai"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
